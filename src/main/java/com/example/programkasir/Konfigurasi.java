0bbfb77fdaec8419cda9cef48118df3c743dd788
GumBall00n <87594435+Gumball00n@users.noreply.github.com>
2023-01-19 22:01:03 +0800

    Add files via upload

---
+++ src/main/java/com/example/programkasir/Konfigurasi.java
@@ -0,0 +1,36 @@
+package com.example.programkasir;
+
+public class Konfigurasi {
+    //menyimpan text text yang berwara hijau ini ke dalam variabel bertipestring
+    public static final String URL_GET_ALL =
+            "http://10.0.2.2/uasppb/tampildata.php";
+    public static final String URL_ADD_DATA =
+            "http://10.0.2.2/uasppb/insert.php";
+    public static final String URL_GET_BAR =
+            "http://10.0.2.2/uasppb/getdata.php?id=";
+    public static final String URL_UPDATE_BAR =
+            "http://10.0.2.2/uasppb/update.php";
+    public static final String URL_DELETE_BAR =
+            "http://10.0.2.2/uasppb/delete.php?id=";
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_KODEBARANG = "kodebarang";
+    public static final String TAG_NAMABARANG = "namabarang";
+    public static final String TAG_HARGABARANG = "hargabarang";
+    public static final String TAG_JUMLAHBARANG = "jumlahbarang";
+    public static final String TAG_DISKONBARANG = "diskonbarang";
+    public static final String TAG_TANGGALBELI = "tanggalbeli";
+    public static final String TAG_NAMAKASIR = "namakasir";
+    public static final String TAG_TOTALBAYAR = "totalbayar";
+    //Dibawah ini merupakan Kunci yang akan digunakan untuk mengirim permintaan ke Skrip PHP
+    public static final String KEY_KODEBARANG = "kodebarang";
+    public static final String KEY_NAMABARANG = "namabarang";
+    public static final String KEY_HARGABARANG = "hargabarang";
+    public static final String KEY_JUMLAHBARANG = "jumlahbarang";
+    public static final String KEY_DISKONBARANG = "diskonbarang";
+    public static final String KEY_TANGGALBELI = "tanggalbeli";
+    public static final String KEY_NAMAKASIR = "namakasir";
+    public static final String KEY_TOTALBAYAR = "totalbayar";
+    //Kode Barang
+    //KB itu artinya Kode Barang
+    public static final String KODEBARANG = "kodebarang";
+}
